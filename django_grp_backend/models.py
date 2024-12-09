33a4a0ded1b4b7088c8e4fa224c4af76d2f29fe6
Jan Civitelli Benso <jan@civitelli.de>
2024-12-09 12:21:30 +0100

    add first models

--- django_grp_backend/models.py
+++ django_grp_backend/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    postalcode = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+
+    def get_full_address(self):
+        return f"{self.address},\n{self.postalcode}, {self.city}"
+
+    def __str__(self):
+        return self.name
+
+
+class Resident(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    picture = models.ImageField()
+    moved_in_since = models.DateField(auto_now_add=True)
+    moved_out_since = models.DateField(default=None, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.get_full_name()
+
+
+class ProtocolItem(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.IntegerField()
+    value = models.TextField()
+
+
+class Protocol(models.Model):
+    protocol_date = models.DateField(auto_now_add=True)
+    date_added = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    exported = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.group.name} - {self.protocol_date}"
