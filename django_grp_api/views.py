64a3723d2d290745f4d1277307f5f3eed20454df
Jan Civitelli Benso <jan@civitelli.de>
2024-12-16 11:49:37 +0100

    implemented presence update via api

--- django_grp_api/views.py
+++ django_grp_api/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from django_grp_backend.models import Protocol, Group, Resident
+from django_grp_backend.models import Protocol, Group, Resident, ProtocolPresence
 from .serializers import ProtocolSerializer, GroupSerializer, ResidentSerializer
 
 
@@ -30,3 +29,25 @@ class ResidentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Resident.objects.all()
     serializer_class = ResidentSerializer
+
+
+class ProtocolPresenceUpdateView(APIView):
+    def post(self, request):
+
+        protocol_id = request.data.get("protocol")
+        user_id = request.data.get("user")
+        was_present = request.data.get("was_present")
+
+        obj, created = ProtocolPresence.objects.update_or_create(
+            protocol_id=protocol_id,
+            user_id=user_id,
+            defaults={"was_present": was_present},
+        )
+
+        return Response(
+            {
+                "message": "Presence updated" if not created else "Presence created",
+                "created": created,
+            },
+            status=status.HTTP_200_OK,
+        )
